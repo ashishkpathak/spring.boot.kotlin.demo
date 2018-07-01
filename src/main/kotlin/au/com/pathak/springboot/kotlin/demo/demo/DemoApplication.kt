0ced421dd3d0f5e5a6c81d971a0e56b4ce653687
Ashish Pathak <ashish.pathak@optus.com.au>
2018-07-02 07:18:00 +1000

    minor updates.

--- src/main/kotlin/au/com/pathak/springboot/kotlin/demo/demo/DemoApplication.kt
+++ src/main/kotlin/au/com/pathak/springboot/kotlin/demo/demo/DemoApplication.kt
@@ -1,11 +1,18 @@
 package au.com.pathak.springboot.kotlin.demo.demo
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class DemoApplication
 
+
+fun print(number: Integer) {
+    print("Hello")
+}
 fun main(args: Array<String>) {
-    runApplication<DemoApplication>(*args)
+//    runApplication<DemoApplication>(*args)
+    SpringApplication.run(DemoApplication::class.java,*args);
+
 }
